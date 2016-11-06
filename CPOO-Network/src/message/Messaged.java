e8246d3622fced136e0d7d5b4c439056316880e8
fchrstl <felix.christelle@isae.fr>
2016-11-06 21:02:34 +0100

    _

--- CPOO-Network/src/message/Messaged.java
+++ CPOO-Network/src/message/Messaged.java
@@ -1,17 +1,17 @@
 package message;
 
 public class Messaged {
-	
+
 	String message;
-	
+
 	public Messaged(String message) {
 		this.message = message;
 	}
-	
+
 	public String getMessage() {
 		return this.message;
 	}
-	
+
 	public void display() {
 		System.out.println(this.message);
 	}
