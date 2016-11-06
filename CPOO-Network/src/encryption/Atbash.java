6840f6258ecbb8f7fe4dabb54b224aa42e4050d5
fchrstl <felix.christelle@isae.fr>
2016-11-06 15:21:38 +0100

    atbash.java

--- CPOO-Network/src/encryption/Atbash.java
+++ CPOO-Network/src/encryption/Atbash.java
@@ -2,22 +2,28 @@ package encryption;
 
 import message.*;
 
+/**
+ * 
+ * @author Benjamin GUIGNARD, Félix CHRISTELLE
+ *
+ */
+
 public class Atbash implements EncryptionAlgorithm {
 
 	public String transform(String str) {
 		StringBuilder strB = new StringBuilder(str);
 		int j;
 		String alphabet = "abcdefghijklmnopqrstuvwxyz";
-		
-		for (int i=0; i<strB.length(); i++) {
+
+		for (int i = 0; i < strB.length(); i++) {
 			j = alphabet.indexOf(strB.charAt(i));
-			if (j>-1) {
-				strB.setCharAt(i, alphabet.charAt(25-j));
+			if (j > -1) {
+				strB.setCharAt(i, alphabet.charAt(25 - j));
 			}
 		}
 		return strB.toString();
 	}
-	
+
 	public Messagec cypher(Messaged md, String key) {
 		return new Messagec(transform(md.getMessage()));
 	}
